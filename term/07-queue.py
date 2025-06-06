d951031c324c2ffcd7d4eb710b3ac21a0cf982f8
Madson <msemsemse4545s@gmail.com>
2025-06-06 15:57:56 +0330

    add: stack

--- term/07-queue.py
+++ term/07-queue.py
@@ -6,11 +6,39 @@ class ListQueue:
 
 
     def enqueue(self,data):
-        self.items.insert(0,data)
+        self.items.insert(0,data) #insert form right
         self.size += 1
 
 
     def dequeue(self):
-        data = self.items.pop()
+        data = self.items.pop() #pop from left
         self.size -= 1
         return data
+
+
+#Stack base queue
+class Queue:
+
+    def __init__(self):
+        self.inbound_stack = []
+        self.outbound_stack = []
+
+
+    def enqueue(self,data):
+        self.inbound_stack.append(data)
+
+
+    def dequeue(self):
+        if not self.outbound_stack:
+            while self.inbound_stack:
+                self.outbound_stack.append(self.inbound_stack.pop())
+        return self.outbound_stack.pop()
+
+
+#test it
+dq = Queue()
+dq.enqueue(10)
+dq.enqueue(20)
+dq.enqueue(30)
+print(dq.inbound_stack)
+print('item1: ', dq.dequeue(), 'item2: ',dq.dequeue())
