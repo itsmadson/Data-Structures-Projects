ce729e3f853fe772edc1608f160f1e072f12de5e
Madson <msemsemse4545s@gmail.com>
2025-06-06 21:53:07 +0330

    add: Node Queues

--- term/08-node-queues.py
+++ term/08-node-queues.py
@@ -15,7 +15,23 @@ class Queue:
 
     def enqueue(self,data):
         node = Node(data,None,None)
-        if self.head is None:
+        if self.head is None: #if list is empty
             self.head = node
             self.tail = self.head
         else:
+            node.prev = self.head
+            self.head.next = node
+            self.head = node
+        self.size += 1
+
+
+    def dequeue(self):
+        current = self.tail
+        if self.size == 1: # if we just have one node
+            self.size -=1
+            self.head = None
+            self.tail = None
+        elif self.size > 1: #if its more than one node
+             self.tail = self.tail.next
+             self.tail.prev = None
+             self.size -= 1
